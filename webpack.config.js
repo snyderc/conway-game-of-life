a8ea8926c24ee6925139198f091521db041e32ca
Chris Snyder <christopher.snyder.m@gmail.com>
2018-09-11 20:59:33 -0400

    Modified for serving live demo with Github Pages

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     entry: './src/app.js',
     output: {
-        path: path.join(__dirname, 'public'),
+        path: path.join(__dirname, 'docs'),
         filename: 'bundle.js'
     },
     module: {
@@ -23,6 +23,6 @@ module.exports = {
     },
     devtool: 'cheap-module-eval-source-map',
     devServer: {
-        contentBase: path.join(__dirname, 'public')
+        contentBase: path.join(__dirname, 'docs')
     }
 };
